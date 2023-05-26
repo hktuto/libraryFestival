6b5fef1a39de0760db0124e27bad6b5edc6f698b
Sean Tsang <9394wing@gmail.com>
2023-05-26 09:31:15 +0800

    init

---
+++ apps/backend/src/api/catergory/controllers/catergory.ts
@@ -0,0 +1,7 @@
+/**
+ * catergory controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::catergory.catergory');
---
+++ apps/backend/src/api/catergory/routes/catergory.ts
@@ -0,0 +1,7 @@
+/**
+ * catergory router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::catergory.catergory');
