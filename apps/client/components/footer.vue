305d79202e7ec2540e5e14d16313250608dd8488
seantsang <seantsang83@gmail.com>
2023-05-27 07:00:57 +0800

    add footer

---
+++ apps/client/components/footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="footerContainer">
+    {{attr.remarkEN}}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {useLang} from "~/composables/lang";
+import {getStrapiData} from "~/utils/helpers";
+
+const { find } = useStrapi()
+
+const {currentLang} = useLang()
+
+const attr = computed<string>(() => getStrapiData(data.value))
+
+const { data, refresh, error } = await useAsyncData(
+    'footerData',
+    () => find('footer')
+)
+</script>
