f598ee2a5c9205a8d9a90345da09e450377d632f
Sean Tsang <9394wing@gmail.com>
2023-06-24 11:15:23 +0800

    add g-tag

--- apps/client/nuxt.config.ts
+++ apps/client/nuxt.config.ts
@@ -1,9 +1,16 @@
+import {isProduction} from "std-env";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     ssr:false,
     css:[
         '~/assets/style.scss'
     ],
+    runtimeConfig:{
+        public: {
+            google_analytics_id: process.env.google_analytics_id,
+            production_mode: isProduction
+        }
+    },
     devtools: true,
     modules: ['nuxt-icon', '@nuxtjs/strapi'],
     strapi: {
