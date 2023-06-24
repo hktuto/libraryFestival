c352a76f978ee9bd07f368aa9ff2e3c0712471d4
Sean Tsang <9394wing@gmail.com>
2023-06-24 12:38:58 +0800

    fix page

--- apps/client/pages/page/[id].vue
+++ apps/client/pages/page/[id].vue
@@ -29,7 +29,7 @@ const post = computed(() => data.value.data[0].attributes)
 const gtag = useGtag()
 gtag('event', 'screen_view', {
   app_name: 'Library Festival',
-  screen_name: data.value.data.attributes.titleEN
+  screen_name: data.value.data[0].attributes.titleEN
 })
 </script>
 
