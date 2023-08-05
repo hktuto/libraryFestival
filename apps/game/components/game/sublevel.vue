30d83c0c01b56d6629c89151e2696c8296abf9ba
Sean Tsang <9394wing@gmail.com>
2023-08-05 18:30:10 +0800

    loop

--- apps/game/components/game/sublevel.vue
+++ apps/game/components/game/sublevel.vue
@@ -21,7 +21,12 @@ function selectedChange(obj:any, value: boolean) {
 
 <template>
     <div class="booksOptions">
-        <GameBook v-for="(answer, index) in answers" :key="answer" :data="answer" @selectedChange="(bool) => selectedChange(answer, bool)" />
+        <img class="betweenImg" v-for="i in 3" :key="i"  :src="`/images/books/${Math.floor(Math.random() * 12) % 10 }.svg`" :style="`--scale:${Math.random() * (1.2 - 0.8) + 0.8}`" />
+        <template v-for="(answer, index) in answers" :key="answer">
+          <GameBook  :data="answer" @selectedChange="(bool) => selectedChange(answer, bool)" />
+          <img class="betweenImg"  :src="`/images/books/${Math.floor(Math.random() * 12) % 10 }.svg`" :style="`--scale:${Math.random() * (1.2 - 0.8) + 0.8}`" />
+        </template>
+      <img class="betweenImg" v-for="i in 3" :key="i"  :src="`/images/books/${Math.floor(Math.random() * 12) % 10 }.svg`" :style="`--scale:${Math.random() * (1.2 - 0.8) + 0.8}`" />
     </div>
     <div class="selectedContainer">
       <div class="selectedBooks">
@@ -46,5 +51,8 @@ function selectedChange(obj:any, value: boolean) {
  }
  .selectedContainer{
    background: #EBCFAC;
+ }
+ .betweenImg{
+   max-height: 200px;
  }
  </style>
